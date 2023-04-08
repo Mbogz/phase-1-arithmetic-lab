c9576fa5bb3c3ab820e4fb188a53ff14a786f287
Mbogz <jeff.mwaura@student.moringaschool.com>
2023-04-08 09:07:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+//multiplication
 let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
+console.log(multiply);
+//output to be 62
+
+//generaterandom integer
+let random = Math.ceil(Math.random() * 100);
+console.log(random);
+
+//variable mod
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+//find highest number
+let set = [2, 5 ,4, 20, 9];
+let max = Math.max(...set);
+console.log(max);
